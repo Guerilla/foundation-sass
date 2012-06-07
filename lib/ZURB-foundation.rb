d16bb33ff5da7e9028c0024ee6effe3122f5364f
zurbchris <chris@zurb.com>
2012-06-07 13:39:51 -0700

    fixed import order for individual imports and bumped version number

--- lib/ZURB-foundation.rb
+++ lib/ZURB-foundation.rb
@@ -3,7 +3,7 @@ Compass::Frameworks.register("ZURB-foundation", :path => File.join(File.dirname(
 
 module ZURBfoundation
 
-  VERSION = "2.2.1.3"
+  VERSION = "2.2.1.4"
   DATE = "2012-06-03"
 
 end
